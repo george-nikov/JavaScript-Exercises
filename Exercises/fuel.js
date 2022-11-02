bf2df16f09383ca935d657c0d2b12f1d8f4bcf91
Georgi Nikov <nikov.george@gmail.com>
2022-11-02 16:05:32 +0200

    Create fuel.js

---
+++ Exercises/fuel.js
@@ -0,0 +1,14 @@
+function speed(averageSpeed, fuelFor100) {
+
+    averageSpeed = Number(averageSpeed);
+    fuelFor100 = Number(fuelFor100);
+
+    let distance = 768800;
+    let time = Math.ceil(distance / averageSpeed);
+    let totalTime = time + 3;
+    let totalFuel = (fuelFor100 * distance) / 100;
+
+    console.log(totalTime);
+    console.log(totalFuel);
+}
+speed(15000,4);
